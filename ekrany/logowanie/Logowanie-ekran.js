acf53a60d5180e52dc2efaace1b5de64877a2a4b
Jan Chojnacki <100374332+Jan-Chojnacki@users.noreply.github.com>
2023-12-10 14:30:16 +0100

    material desing nowy

---
+++ ekrany/logowanie/Logowanie-ekran.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Button} from "react-native";
+import {View} from "react-native";
+
+function LogowanieEkran() {
+    return (
+        <View>
+            <Button title={"Test"}></Button>
+        </View>
+    )
+}
+
+export default LogowanieEkran;
