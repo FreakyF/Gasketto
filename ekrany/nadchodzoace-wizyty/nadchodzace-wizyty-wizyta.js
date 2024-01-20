341729adfa5bf4845361844fd12bb2a251f98506
Jakub Babiarski <116975073+JakubKross@users.noreply.github.com>
2024-01-20 22:55:01 +0100

    Przygotowanie ekranu nadchodzace-wizyty-wizyta.js do wprowadzenia trybu ciemnego

--- ekrany/nadchodzoace-wizyty/nadchodzace-wizyty-wizyta.js
+++ ekrany/nadchodzoace-wizyty/nadchodzace-wizyty-wizyta.js
@@ -34,3 +34,23 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
     }
 });
+
+const Darkstyles = StyleSheet.create({
+    container: {
+        backgroundColor: '#30302f',
+        borderColor: '#bdbab3',
+        borderWidth: 1,
+        minWidth: '85%',
+        borderRadius: 10,
+        margin: 10,
+        padding: 7,
+    },
+    text: {
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        color: '#cfcecc',
+    },
+    bold: {
+        fontWeight: "bold",
+    }
+});
