0aa26a3341b08caf7ca404900aa2fef388d2d18a
Jakub Babiarski <116975073+JakubKross@users.noreply.github.com>
2024-01-21 18:19:56 +0100

    Wporwadzenie motywu zmieniającego się pod wpływem działania czujnika światła w ekranie nadchodzace-naprawy-ekran.js

--- ekrany/nadchodzoace-naprawy/nadchodzace-naprawy-ekran.js
+++ ekrany/nadchodzoace-naprawy/nadchodzace-naprawy-ekran.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 import Naprawa from "./nadchodzace-naprawy-wizyta";
 import Linia from "../../ui/Linia";
 import Tytul from "../../ui/Tytul";
+import {LightSensor} from "expo-sensors";
 
 function NadchodzaceNaprawyEkran({navigation}) {
+
+    const [illuminance, setilluminance] = useState(0);
+    const aktywnystyl = illuminance > 25 ? styles : Darkstyles ;
+    LightSensor.addListener(data => { setilluminance(data.illuminance)})
+
     return (
-        <View style={styles.container}>
+        <View style={aktywnystyl.container}>
             <Tytul text={"Nadchodzące naprawy"}/>
             <Linia text="Dzisiaj"/>
             <Naprawa navigation={navigation}/>
