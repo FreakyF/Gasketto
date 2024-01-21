9f949cfbd4a7883afcf66ab8f0c9a5639eb916ee
Kamil Fudala <kamil.fudala44@gmail.com>
2024-01-21 21:52:18 +0100

    Naprawiono placeholder texty.

--- ekrany/Home.js
+++ ekrany/Home.js
@@ -1,31 +1,38 @@
-import 'react-native-gesture-handler'
-import {createDrawerNavigator} from "@react-navigation/drawer";
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import Nadchodzace_wizyty_stack from "./nadchodzoace-wizyty/nadchodzace-wizyty-stack";
 import Nadchodzace_naprawy_stack from "./nadchodzoace-naprawy/nadchodzace-naprawy-stack";
-import {StyleSheet} from "react-native";
-import React, {useEffect, useState} from "react";
-import {LightSensor} from "expo-sensors";
+import React, { useEffect, useState } from "react";
+import { LightSensor } from "expo-sensors";
 
 function Home() {
     const Drawer = createDrawerNavigator();
-    const [illuminance, setilluminance] = useState(0);
-    const zmianatla = illuminance > 25 ? '#fff' : '#000';
-    LightSensor.addListener(data => {setilluminance(data.illuminance)})
+    const [illuminance, setIlluminance] = useState(0);
+    const zmianatla = illuminance > 25 ? '#fff' : '#000';  // Kolor tła
+    const tekstKolor = illuminance > 25 ? '#000' : '#fff';  // Kolor tekstu
+
+    useEffect(() => {
+        const subscription = LightSensor.addListener(data => {
+            setIlluminance(data.illuminance);
+        });
+
+        return () => {
+            subscription.remove();
+        };
+    }, []);
+
     return (
-            <Drawer.Navigator  initialRouteName="Wizyty" screenOptions={{swipeEdgeWidth: 50, drawerStyle: {backgroundColor: zmianatla}}}>
-                <Drawer.Screen name="Wizyty" component={Nadchodzace_wizyty_stack} options={{headerShown: false,}}/>
-                <Drawer.Screen name="Naprawy" component={Nadchodzace_naprawy_stack} options={{headerShown: false}}/>
-            </Drawer.Navigator>
-    )
+        <Drawer.Navigator
+            initialRouteName="Wizyty"
+            screenOptions={{
+                swipeEdgeWidth: 50,
+                drawerStyle: { backgroundColor: zmianatla },
+                drawerLabelStyle: { color: tekstKolor }, // Dodane
+            }}>
+            <Drawer.Screen name="Wizyty" component={Nadchodzace_wizyty_stack} options={{headerShown: false}}/>
+            <Drawer.Screen name="Naprawy" component={Nadchodzace_naprawy_stack} options={{headerShown: false}}/>
+        </Drawer.Navigator>
+    );
 }
 
 export default Home;
-
-const styles = StyleSheet.create({
-    drawerStyle: {
-        backgroundColor: '#fff',
-    }})
-const Darkstyles = StyleSheet.create({
-    drawerStyle: {
-        backgroundColor: '#000',
-    }})
